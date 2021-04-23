1998a3e143e480d05b42515b1fcd106dd0594f93
sleepypingu <kardildaryl@gmail.com>
2021-04-23 15:30:16 +0800

    object oriented

---
+++ app/src/main/java/com/example/objectorientedoopsies/Measurements.java
@@ -0,0 +1,14 @@
+package com.example.objectorientedoopsies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Measurements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurements);
+    }
+}
